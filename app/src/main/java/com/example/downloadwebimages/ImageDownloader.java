80daf407f4938f212371825fa768be6d5523220c
Daniel <danielchua93@gmail.com>
2022-01-09 16:16:59 +0800

    tried moving some methods to ImageDownloader class

--- app/src/main/java/com/example/downloadwebimages/ImageDownloader.java
+++ app/src/main/java/com/example/downloadwebimages/ImageDownloader.java
@@ -1,5 +1,9 @@
 package com.example.downloadwebimages;
 
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -8,7 +12,49 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class ImageDownloader {
-    public ArrayList<String> imgSrcs;
+
+    private WebView mWebView;
+    private String externalUrl;
+
+    protected void loadPage() {
+        mWebView.setWebViewClient(new WebViewClient() {
+                                      @Override
+                                      public void onPageFinished(WebView view, String url) {
+                                          super.onPageFinished(view,url);
+                                          System.out.println("page finished loading");
+
+                                          // Begin download process
+                                          getImgURLs(mWebView);
+
+
+
+                                      }
+                                  }
+        );
+        System.out.println("External URL in loadPage() = " + externalUrl);
+        mWebView.loadUrl(externalUrl);
+
+    }
+
+    public void getImgURLs(WebView webView) {
+        String javascriptFn = "(function() {let imageNodes = document.querySelectorAll(\"img\"); " +
+                "let imageSrcs = new Array(); " +
+                "imageNodes.forEach(imageNode => imageSrcs.push(imageNode.src));" +
+                "return imageSrcs; })()";
+        webView.evaluateJavascript(javascriptFn, new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String s) {
+                System.out.println("Callback for getImgURLs");
+                externalUrl = null;
+                imageURLs = s;
+                printImageURLs();
+                processImageUrlString();
+                downloadImages();
+
+
+            }
+        });
+
 
 
     protected boolean downloadImage(String imgURL, File destFile) {
